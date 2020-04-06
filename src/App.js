6cfc3ffcab6264c65eae32ac20bddf7bbafdec43
drusilla-mbabazi <drusymbabazi@gmail.com>
2020-04-05 22:31:35 -0700

    my app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Drusilla and Lauren";
+  const [imageUrl, setImageUrl] = useState(
+    "https://media3.giphy.com/media/11wsZr0jbXc15m/giphy.gif?cid=790b76110b2799c8c87b11e5add6f9ce623997dd1fe0852e&rid=giphy.gif"
+  );
+  function handleClick() {
+    fetch(
+      "http://api.giphy.com/v1/gifs/random?api_key=aHi5kZ49h4esZTalKqyvkBO2llvuHeJr&tag=funny"
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        const newImageUrl = myJson.data.images.original.url;
+        setImageUrl(newImageUrl);
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>{name}'s Shuffle app!</h1>
+        <img src={imageUrl} className="App-logo" alt="logo" />
+        <p>This app was created by {name}</p>
+         
+        <button onClick={handleClick}>Shuffle!</button>
       </header>
     </div>
   );
